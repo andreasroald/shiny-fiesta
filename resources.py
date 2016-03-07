c90a1d5a538f964dfba3dfeee0258b2797928232
andreasroald <andreas.roald@nettbrev.com>
2016-03-07 16:13:51 +0100

    Update resources.py

--- resources.py
+++ resources.py
@@ -20,25 +20,3 @@ player_list = [ player_standing, player_walk_1, player_walk_2, player_walk_3, pl
                 player_walk_5, player_walk_6, player_walk_7, player_walk_8,
                 player_walk_9, player_walk_10, player_walk_11, player_walk_12]
 
-# NPC images
-NPC_standing = pygame.image.load("sprites/NPC/NPC_standing.png")
-
-NPC_walk_1 = pygame.image.load("sprites/NPC/NPC_walk_1.png")
-NPC_walk_2 = pygame.image.load("sprites/NPC/NPC_walk_2.png")
-NPC_walk_3 = pygame.image.load("sprites/NPC/NPC_walk_3.png")
-NPC_walk_4 = pygame.image.load("sprites/NPC/NPC_walk_4.png")
-NPC_walk_5 = pygame.image.load("sprites/NPC/NPC_walk_5.png")
-NPC_walk_6 = pygame.image.load("sprites/NPC/NPC_walk_6.png")
-NPC_walk_7 = pygame.image.load("sprites/NPC/NPC_walk_7.png")
-NPC_walk_8 = pygame.image.load("sprites/NPC/NPC_walk_8.png")
-NPC_walk_9 = pygame.image.load("sprites/NPC/NPC_walk_9.png")
-NPC_walk_10 = pygame.image.load("sprites/NPC/NPC_walk_10.png")
-NPC_walk_11 = pygame.image.load("sprites/NPC/NPC_walk_11.png")
-NPC_walk_12 = pygame.image.load("sprites/NPC/NPC_walk_12.png")
-
-NPC_list = [ NPC_standing, NPC_walk_1, NPC_walk_2, NPC_walk_3, NPC_walk_4,
-                NPC_walk_5, NPC_walk_6, NPC_walk_7, NPC_walk_8,
-                NPC_walk_9, NPC_walk_10, NPC_walk_11, NPC_walk_12]
-
-# GUI images
-mouse_pointer = pygame.image.load("sprites/gui/pointer.png")
